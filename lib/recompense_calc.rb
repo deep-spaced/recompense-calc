0a08f5363a90bfb9443c827ede1477e0abf76b07
Andrew Anderson <gimlilongbow@gmail.com>
2018-08-30 23:07:41 -0400

    Removing early abstraction, more tests.

--- lib/recompense_calc.rb
+++ lib/recompense_calc.rb
@@ -1,4 +1,4 @@
-require 'project'
+require 'date'
 
 class RecompenseCalc
 
@@ -6,16 +6,50 @@ class RecompenseCalc
     @projects = []
   end
 
+  def projects
+    @projects
+  end
+
   def add_project(rate, start_date, end_date)
-    @projects.push(Project.new(rate, start_date, end_date))
+    @projects.push generate_project(rate, start_date, end_date)
   end
 
   def total
-    @projects.reduce(0) {|total, p| total += p.cost }
+    calculate_costs(@projects)
   end
 
-  def to_s
-    calculate.to_s
+  private
+
+  def generate_project(rate, start_date, end_date)
+    parsed_start = Date.parse(start_date)
+    parsed_end = Date.parse(end_date)
+
+    total_days = (parsed_start..parsed_end).count
+    travel_days = total_days >= 2 ? 2 : 0
+
+    {
+      rate: rate.to_i,
+      start_date: parsed_start,
+      end_date: parsed_end,
+      travel_days: travel_days,
+      full_days: total_days - travel_days
+    }
   end
 
+  def calculate_costs(projects)
+    cost_info = {}
+    projects.each do |p|
+      cost_info[p[:rate]] = cost_info[p[:rate]] || {travel: 0, full: 0}
+
+      cost_info[p[:rate]][:travel] += p[:travel_days]
+      cost_info[p[:rate]][:full] += p[:full_days]
+    end
+
+    total = 0
+    cost_info.each_pair do |rate, days|
+      total += (days[:full] * rate) + (days[:travel] * (rate.to_f/2))
+    end
+
+    total
+  end
 end
--- tests/test_recompense_calc.rb
+++ tests/test_recompense_calc.rb
@@ -3,6 +3,14 @@ require "test/unit"
 
 class TestRecompenseCalc < Test::Unit::TestCase
 
+  def test_add_project
+    calc = RecompenseCalc.new
+    calc.add_project(75, "2015-09-01", "2015-09-03")
+    calc.add_project(75, "2015-09-01", "2015-09-03")
+
+    assert_equal(calc.projects.size, 2)
+  end
+
   def test_single_project
     calc = RecompenseCalc.new
     calc.add_project(75, "2015-09-01", "2015-09-03")
@@ -10,4 +18,43 @@ class TestRecompenseCalc < Test::Unit::TestCase
     assert_equal(150, calc.total)
   end
 
+  def test_two_projects
+    calc = RecompenseCalc.new
+    calc.add_project(45, "2015-09-01", "2015-09-03")
+    calc.add_project(75, "2015-09-05", "2015-09-07")
+    # Travel at 45: 2
+    # Full days at 45: 1
+    # Travel days at 75: 2
+    # Full days at 75: 1
+    # Total: 240
+
+    assert_equal(240, calc.total)
+  end
+
+  def test_three_projects
+    calc = RecompenseCalc.new
+    calc.add_project(45, "2015-09-01", "2015-09-01")
+    calc.add_project(75, "3015-09-02", "2015-09-07")
+    calc.add_project(45, "2015-09-09", "2015-09-11")
+    # Travel at 45: 2
+    # Full days at 45: 2
+    # Travel days at 75: 2
+    # Full days at 75: 4
+    # Total:
+
+    # assert_equal(510, calc.total)
+  end
+
+  def test_three_projects_overlap
+    calc = RecompenseCalc.new
+    calc.add_project(45, "2015-09-01", "2015-09-01")
+    calc.add_project(75, "2015-09-02", "2015-09-06")
+    calc.add_project(45, "2015-09-06", "2015-09-08")
+    # Travel at 45: 1
+    # Full days at 45: 2
+    # Full days at 75: 5
+    # Total: 375+112.5 = 487.5
+
+    # assert_equal(487.5, calc.total)
+  end
 end
